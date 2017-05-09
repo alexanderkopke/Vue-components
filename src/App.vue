c192c0ae7c46b827bed0596c907a1b131099cff7
Alexander Køpke <alexanderkopke@outlook.com>
2017-05-09 02:20:02 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <div class="container-fluid">
+    <ul>
+    <post :post="post" v-for="post in posts"  :key="post.title"></post>
+    </ul>
+    <div id="comment-box">
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="Enter a comment..." v-model="comment" @keyup.enter="postComment">
+        <span class="input-group-btn">
+        <button class="btn btn-primary" type="button" @click="postComment">Submit</button></span>
+    </div>
+  </div>
+    </div>
+  </div>
+</template>
+<script>
+import Post from './components/Post.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+     posts: [
+          { title: "A post for the reddit demo starting at 15 votes",
+          votes: 15},
+          {
+            title: "Try out the upvoting, it works, I promise",
+            votes: 63
+          },
+          {
+            title: "Alexander rules some kind of world", 
+            votes: 118
+          }
+     ],
+     comment:''
+    }
+  },
+  methods:{
+    postComment(){
+      this.posts.push({
+        title: this.comment,
+        votes: 0
+      })
+      this.comment = ''
+    }
+  },
+  components:{'post': Post}
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0 auto;
+  margin-top: 60px;
+  width: 600px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+
+}
+
+a {
+  color: #42b983;
+}
+
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="post">
+    <li class="list-group-item">
+      <i class="glyphicon glyphicon-chevron-up" @click="upvote" :class="{disabled: upvoted}"></i>
+      <span class="label label-primary">{{ votes }}</span>
+      <i class="glyphicon glyphicon-chevron-down" @click="downvote" :class="{disabled: downvoted}"></i>
+      <a>{{ post.title }}</a>
+    </li>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      upvoted: false,
+      downvoted: false
+    }
+  },
+  methods:{
+    upvote(){
+      this.upvoted =! this.upvoted;
+      this.downvoted= false;
+    },
+    downvote(){
+      this.downvoted =! this.downvoted;
+      this.upvoted= false;
+    }
+  },
+  computed: {
+    votes(){
+      if(this.upvoted){
+        return this.post.votes + 1;
+      } else if(this.downvoted){
+        return this.post.votes - 1; 
+      } else{
+        return this.post.votes;
+      }
+    }
+  }
+  ,
+  props:['post']
+  
+}
+
+</script>
+
+
+<style>
+.disabled{
+  color: orange
+}
+#post{
+  text-align:left;
+ 
+
+}
+
+</style>
